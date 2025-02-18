d62c12ed3bc7e4c1909fd6b08cab4fed9bf6cdf6
Tompa <itom1246@hotmail.com>
2025-02-18 12:02:56 +0100

    wagon Graphics
    Using my P-uppgift version 4 code in my
    p-upgift version 7
    to print a train wagon. I'm gonna try to make it more simple
    
    to make the code work I changed variables name's = rad , kolumn.
    and change index plats .....

--- P-upgift_7.py
+++ P-upgift_7.py
@@ -2,14 +2,30 @@ import os
 
 class Train:
     def __init__(self, row, column):
+        self.plats_info = [{"seatnumber":n+1, "status":False, "name":None } for n in range(row * column)]
         self.row = row
         self.column = column
         
     def printwagon(self):
-        for x in range(self.row):
-            for y in range(self.column):
-                print(1, end=" ")
-            print()
+        for row in range(self.row):
+            print("|", end=" ")
+            
+            for column in range(self.column):
+                plats = row * self.column + column * 1
+                
+                if plats > len(self.plats_info):
+                    print("   ", end=" ")
+                
+                else:
+                    print(f"{plats:02d}", end="  ")
+            print("|")
+            
+            # lägger en gång_väg i mellan rad index 1 som är på rad 2
+            if row == 1:
+                gångväg_bredd = self.column * 4 + 1
+                print("|" + "-" * gångväg_bredd + "|")
+                print("|" + "TYST AVD".center(gångväg_bredd) + "|")
+                print("|" + "-" * gångväg_bredd + "|")
     
 
 class BookingSystem:
